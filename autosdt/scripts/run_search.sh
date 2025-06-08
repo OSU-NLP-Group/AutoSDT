46358e628e84c64717ecc60b8b8abed04a625462
hananemoussa <moussa.45@buckeyemail.osu.edu>
2025-06-08 10:06:21 -0700

    Update run_search.sh

--- autosdt/scripts/run_search.sh
+++ autosdt/scripts/run_search.sh
@@ -1,4 +1,4 @@
-python ../src/AutoSDT-Search.py \
+python ../src/autosdt_search.py \
 --llm_engine_name struct_azure_gpt-4o \
---output_csv ../../repo_list/repositories.csv \
+--output_csv ../result/repositories.csv \
 --api_version 2025-01-01-preview
