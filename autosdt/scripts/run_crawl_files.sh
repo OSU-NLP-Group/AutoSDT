4b2c5adc10eb85b37924366eece67c7aa92e9238
hananemoussa <moussa.45@buckeyemail.osu.edu>
2025-06-08 10:03:58 -0700

    Update run_crawl_files.sh

--- autosdt/scripts/run_crawl_files.sh
+++ autosdt/scripts/run_crawl_files.sh
@@ -1,4 +1,4 @@
-python ../src/crawl_scientific_py_files.py \
---csv_file ../../repo_list/final_repos.csv \
+python ../src/autosdt_select_crawl_files.py \
+--csv_file ../result/repositories.csv \
 --repo_base_dir ../../downloaded_repos \
 --output_file ../result/python_files.jsonl
