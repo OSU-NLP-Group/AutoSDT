840585321ff8bb9c281ef3fe649795789845c649
hananemoussa <moussa.45@buckeyemail.osu.edu>
2025-06-11 10:33:16 -0700

    Update run_search.sh

--- autosdt/scripts/run_search.sh
+++ autosdt/scripts/run_search.sh
@@ -1,4 +1,5 @@
 python ../src/autosdt_search.py \
 --llm_engine_name struct_azure_gpt-4o \
 --output_csv ../result/repositories.csv \
---api_version 2025-01-01-preview
+--api_version 2025-01-01-preview \
+--base_keywords "bioinformatics, psychology, neuroscience"
